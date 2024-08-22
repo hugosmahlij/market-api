76ad11ad08b739fe902e690717125a367b08cd20
Hugo <smahlij@gmail.com>
2024-08-22 17:13:03 -0300

    agregado de calculo de calorias de bebidas

--- src/main/java/com/market/servicio/ServicioProducto.java
+++ src/main/java/com/market/servicio/ServicioProducto.java
@@ -3,6 +3,7 @@ package main.java.com.market.servicio;
 import main.java.com.market.excepciones.ExcesoDeStockException;
 import main.java.com.market.excepciones.SaldoInsuficienteException;
 import main.java.com.market.modelo.Producto;
+import main.java.com.market.modelo.ProductoBebida;
 import main.java.com.market.modelo.Tienda;
 
 import java.math.BigDecimal;
@@ -11,6 +12,7 @@ import java.util.List;
 
 public class ServicioProducto {
 
+    // Metodo para agregar productos a la tienda
     public void agregarProductoATienda(Producto producto, Tienda tienda) {
         BigDecimal costoTotalProductos = producto.getPrecioUnitario().multiply(new BigDecimal(producto.getStock()));
         Double saldoDeCaja = tienda.getSaldoCaja() - costoTotalProductos.doubleValue();
@@ -26,4 +28,17 @@ public class ServicioProducto {
         producto.setEstaDisponibleVenta(true);
         tienda.getListaProductos().add(producto);
     }
-}
+
+    //Metodo para calcular las calorías de las bebidas
+    private Integer calcularCalorias(ProductoBebida bebida) {
+        Double graduacion = bebida.getGraduacionAlcohol();
+        Integer calorias = bebida.getCalorias();
+
+        if (graduacion <= 2) { 
+            return calorias;
+        } else if (graduacion <= 4.5 ) {
+            return (int) (calorias * 1.25);
+        } else  {
+            return (int) (calorias * 1.5);
+        }
+    } 
